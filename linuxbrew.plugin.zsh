1b3c11dfb800051c14eb85d425af42801627465d
Grigorii Horos <horosgrisa@gmail.com>
2017-04-11 15:43:35 +0300

    Initial commit

---
+++ linuxbrew.plugin.zsh
@@ -0,0 +1,13 @@
+#!/usr/bin/env zsh
+
+if [[ ! -d "$HOME/.linuxbrew" ]]; then
+  git clone https://github.com/Linuxbrew/brew ~/.linuxbrew </dev/null >/dev/null 2>/dev/null &!
+fi
+
+if [[ -d "$HOME/.linuxbrew" ]]; then
+  export PATH="$HOME/.linuxbrew/bin:$PATH"
+  export MANPATH="$HOME/.linuxbrew/share/man:$MANPATH"
+  export INFOPATH="$HOME/.linuxbrew/share/info:$INFOPATH"
+  fpath=( "$HOME/.linuxbrew/completions/zsh" $fpath )
+fi
+
